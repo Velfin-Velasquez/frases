0fe9579975963ad8bf82076deee60886a0ff9835
Velfin Velasquez <vvelasquez2465@utm.edu.ec>
2020-12-12 08:02:04 -0500

    se agrego boton para obtener frase

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React from "react";
+import styled from "@emotion/styled";
+
+const Contenedor = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
+`;
+
+const Boton = styled.button`
+  background: #007d35;
+  background-size: 300px;
+  font-family: Arial, Helvetica, sans-serif;
+  color: #fff;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+`;
+
 
 export const App = () => {
+
+  const consultarAPI = ()=>{
+    console.log("escribiendo..");
+  }
+
   return (
-    <div>
-      <h1>Frases</h1>
-    </div>
+    <Contenedor>
+    <Boton
+    onClick={consultarAPI}
+    >
+      Obtener Frase
+      </Boton>
+  </Contenedor>
   )
 }
